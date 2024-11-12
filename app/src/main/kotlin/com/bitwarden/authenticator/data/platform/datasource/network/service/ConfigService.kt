6df2149aa0288d86e4a8d6e0f464433a9f5b808a
André Bispo <abispo@bitwarden.com>
2024-11-12 11:57:55 +0000

    [BWA-86] Add kdoc for ConfigService

--- app/src/main/kotlin/com/bitwarden/authenticator/data/platform/datasource/network/service/ConfigService.kt
+++ app/src/main/kotlin/com/bitwarden/authenticator/data/platform/datasource/network/service/ConfigService.kt
@@ -3,7 +3,7 @@ package com.bitwarden.authenticator.data.platform.datasource.network.service
 import com.bitwarden.authenticator.data.platform.datasource.network.model.ConfigResponseJson
 
 /**
- * Provides an API for querying config endpoints.
+ * Provides an API for querying for app configurations.
  */
 interface ConfigService {
 
